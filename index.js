ab0ef32d990a1c550715011c1f529444d5588c97
Mithun <mrmithunofficial321@gmail.com>
2023-09-21 15:45:58 +0530

    conflict second commit

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ console.log('index');
 <<<<<<< HEAD
 console.log('version 12');
 =======
-console.log('version 13');
+console.log('version 14');
 >>>>>>> 2a4573d99df08df026b9ca40cde1ae81c9236b62
--- script.js
+++ script.js
@@ -3,5 +3,5 @@ console.log('script');
 console.log('version 12');
 =======
 console.log('script');
-console.log('version 13');
+console.log('version 14');
 >>>>>>> 2a4573d99df08df026b9ca40cde1ae81c9236b62
